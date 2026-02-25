53c9f19d1948927bb6b6b1b3577b47fe26087abc
Radu Bogdan <bogdan88.radu@gmail.com>
2026-02-25 17:26:15 +0200

    Lab 1: HTML and CSS review

---
+++ contact-form.js
@@ -0,0 +1,17 @@
+function submitForm() {
+
+    const nume = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const mesaj = document.getElementById("message").value;
+
+    console.log("Nume:", nume);
+    console.log("Email:", email);
+    console.log("Mesaj:", mesaj);
+
+    console.warn("Goodbye World!");
+
+
+    // const acum=new Date();
+    // const ora=now.getHours();
+    // const Header=document.querySelector("")
+}
