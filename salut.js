98cb4e5300faa6f2904ce3d6ee37ad609f9d84e6
Radu Bogdan <bogdan88.radu@gmail.com>
2026-02-25 17:50:29 +0200

    feat: muta scriptul in fisier JS separat

---
+++ salut.js
@@ -0,0 +1,11 @@
+const acum=new Date();
+     const ora=now.getHours();
+     const Header=document.querySelector('header p');
+
+     if(ora>=6 && ora<=11){
+        Header.textContent="Bună dimineața!";
+        }else if(ora>=12 && ora<=18){
+            Header.textContent="Bună ziua!";
+        }else {
+            Header.textContent="Bună seara!";
+        }
