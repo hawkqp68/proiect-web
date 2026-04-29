86058ca7ccc9078bdbd80b5ebdf97f686ce74d4b
Radu Bogdan <bogdan88.radu@gmail.com>
2026-04-29 15:23:55 +0300

    Rescriere GET cu MongoDB

--- server/index.js
+++ server/index.js
@@ -10,38 +10,38 @@ mongoose.connect('mongodb://localhost:27017/dashboard')
  });
 
 const PORT=3000;
+const Project = require('./models/Project');
 
 app.use(express.json());
 // Prima ruta: raspunde la GET /
 app.get('/', function(req, res) {
  res.json({ message: 'Serverul functioneaza!' });
 });
-// Date (temporar in memorie, vom folosi MongoDB mai tarziu)
-const projects = [
- { id: 1, title: "Pagina Personala", tech: "HTML, CSS", done: true },
- { id: 2, title: "Calculator Buget", tech: "JS", done: true },
- { id: 3, title: "Dashboard React", tech: "React", done: false },
- { id: 4, title: "API Meteo", tech: "React, API", done: false },
-];
+
 // GET /api/projects - returneaza toate proiectele
-app.get('/api/projects', function(req, res) {
+app.get('/api/projects', async function(req, res) {
+ try {
+ const projects = await Project.find();
  res.json(projects);
+ } catch (err) {
+ res.status(500).json({ error: 'Eroare ' + err });
+ }
 });
 
-app.get('/api/projects/:id', function(req, res) {
-    const project = projects.find(p => p.id === parseInt(req.params.id));
-    if(!project) { return res.status(404).json({ error: 'Proiectul nu a fost gasit' }); }
-    res.json(project);
-});
+//app.get('/api/projects/:id', function(req, res) {
+  //  const project = projects.find(p => p.id === parseInt(req.params.id));
+    //if(!project) { return res.status(404).json({ error: 'Proiectul nu a fost gasit' }); }
+    //res.json(project);
+//});
 
 
-app.get('/api/stats', function(req, res) {
-  res.json({
-    total: projects.length,
-    done: projects.filter(p => p.done).length,
-    inProgress: projects.filter(p => !p.done).length,
-  });
-});
+//app.get('/api/stats', function(req, res) {
+  //res.json({
+    //total: projects.length,
+    //done: projects.filter(p => p.done).length,
+    //inProgress: projects.filter(p => !p.done).length,
+  //});
+//});
 
 app.post('/api/projects', function(req, res)
 {
