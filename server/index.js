4cb0c737d597d2c1f849844e19d785052e1e4cb3
Radu Bogdan <bogdan88.radu@gmail.com>
2026-05-06 14:31:05 +0300

    Activarea CORS pe server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/dashboard')
